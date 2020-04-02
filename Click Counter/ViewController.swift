6978cb535ae4abcc199b28953429bdabb068f6ae
aftiyah <abd2010ly@gmail.com>
2020-04-02 16:44:48 +0200

    Transitioning to Storyboard, Button

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     }
     
 
-    @objc func incrementCount()   {
+    @IBAction func incrementCount()   {
         self.count += 1
         self.label.text = "\(self.count)"
 
