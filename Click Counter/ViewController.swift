aacf422be62be538f34da9955b65ee5bb415a950
aftiyah <abd2010ly@gmail.com>
2020-04-02 16:12:35 +0200

    Add an additional label and have it increment with the first

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -10,24 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     var count = 0
-    var label = UILabel()
+    var label1 = UILabel()
+    var label2 = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // label
-        let label = UILabel()
+        let label1 = UILabel()
+        let label2 = UILabel()
         // button
         let button = UIButton()
-        createFormatter(label: label, button: button)
+        createFormatter(label1: label1, label2: label2, button: button)
     }
     
 
-    func createFormatter(label:UILabel, button:UIButton) {
+    func createFormatter(label1:UILabel,label2:UILabel, button:UIButton) {
     
-        label.frame = CGRect(x: 200, y: 200, width: 60, height: 60)
-        label.text = "0"
-        view.addSubview(label)
-        self.label = label
+        label1.frame = CGRect(x: 150, y: 200, width: 60, height: 60)
+        label1.text = "0"
+        view.addSubview(label1)
+        self.label1 = label1
+        
+        label2.frame = CGRect(x: 250, y: 200, width: 60, height: 60)
+        label2.text = "0"
+        view.addSubview(label2)
+        self.label2 = label2
         
         button.frame = CGRect(x: 150, y: 300, width: 100, height: 60)
         button.setTitle("Click", for: .normal)
@@ -40,7 +47,8 @@ class ViewController: UIViewController {
     
     @objc func incrementCount()   {
         self.count += 1
-        self.label.text = "\(self.count)"
+        self.label1.text = "\(self.count)"
+        self.label2.text = "\(self.count)"
     }
 
 
