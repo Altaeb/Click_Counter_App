b70d1a5c3c2f5b8f0baf28fca1a3fb0740070a26
aftiyah <abd2010ly@gmail.com>
2020-04-02 15:29:45 +0200

    Add UIViews from code.

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -13,7 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createFormatter()
     }
+    
+    // label
+    let label = UILabel()
+    // button
+    let button = UIButton()
+    
+    func createFormatter() {
+    
+        label.frame = CGRect(x: 200, y: 200, width: 60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        
+        button.frame = CGRect(x: 150, y: 300, width: 100, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
+        
+    }
+    
+
 
 
 }
